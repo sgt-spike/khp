10516c0dff2e799afd5007fc39655881f8a6ae45
Brad <btchriss@gmail.com>
2019-10-19 17:39:32 -0400

    Implement SASS styling sheet to main.css

--- header.php
+++ header.php
@@ -4,14 +4,14 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <link rel="stylesheet" href="css/header.css">
+   <!-- <link rel="stylesheet" href="css/header.css"> -->
    <link rel="stylesheet" href="css/main.css">
    <link rel="stylesheet" href="css/stories.css">
    <link rel="stylesheet" href="css/main-whoweare.css">
    <link rel="stylesheet" href="css/contact.css">
    <link rel="stylesheet" href="css/sponsors.css">
    <!-- <link rel="stylesheet" href="css/main-founder.css"> -->
-   <link rel="stylesheet" href="css/footer.css">
+   <!-- <link rel="stylesheet" href="css/footer.css"> -->
    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
    <!-- need to show handprint icon on tab -->
    <title>Katie's Handprint</title>
