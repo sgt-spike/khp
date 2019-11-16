aa97e057c521e7ede3c4b7f4e6fb3c7e1aec88d1
Brad <btchriss@gmail.com>
2019-11-16 09:42:30 -0500

    Coding Contact Us page

--- contact.php
+++ contact.php
@@ -1,24 +1,12 @@
 <?php
    require 'header.php';
-   //require 'php/sendmessage.php';
+   //require 'php/handle_email.php';
 
-   $errors = array();
-   $sent = false;
+   ini_set('display_errors', 1);
+   error_reporting(E_ALL);
 
-   if (! empty($_POST)) {
-      $process = process_form($_POST);
-
-      //Check for Errors
-      if (! empty($process['message'])){
-         $errors[] = $process['message'];
-      } else if (! empty($process['errors'])){
-         $errors = $process['errors'];
-      } else {
-         $sent = true;
-      }
-   }
 ?>
-   <main class="container">
+   <main class="container contact-container">
       <h1 id="contact-h1" class="main-h1">Contact Us</h1>
       <div id="contact-form">
          <div id="form-heading">
@@ -26,36 +14,55 @@
             <p>Leave us a message</p>
          </div>
          <div id="form-errors">
+            <!-- PHP if statement to determine errors.  If so print the errors, otherwise print success message -->
+            <!-- If there are errors, print the errors -->
+            <?php if ( ! empty( $errors ) ) : ?>
             <div class="errors">
-               <p class="bg-danger"></p>
+               <!-- Prints errors to the page -->
+               <?php echo implode( '.</p><p class="bg-danger">', $errors ); ?>.
             </div>
-            <!-- <script>
-               document.querySelector('#form-heading').style.display = 'none';
-               document.querySelector('#form-errors').style.display = "initial";
-               </script> -->
+            <!-- put display element script here again -->
+            <!-- If message sent without errors print success message -->
+            <?php elseif ( $sent ) : ?>
             <div class="success">
-               <p class="bg-success">Your message was sent. We'll be in touch.</p>
+               <p class="bg-success">Thank You! Your message was sent. We'll be in touch.</p>
             </div>
-            <!-- <script>
+            <!-- When a message is sent this script displays the form error element -->
+            <script>
                document.querySelector('#form-heading').style.display = 'none';
                document.querySelector('#form-errors').style.display = "initial";
-               </script> -->
+            </script>
+            <?php endif; ?>
          </div>
          <form action="#" method="post" class="form" name="contact-form" target="_self">
-               <label for="name">Full Name</label><br>
-               <input type="text" name="name" id="form-name" placeholder="Full Name" value=""><br><br>
-               <label for="email">Email Address</label><br>
-               <input type="email" name="email" id="form-email" placeholder="Email Address" value=""><br><br>
-               <label for="subject">Subject</label><br>
-               <input type="text" name="subject" id="form-subject" placeholder="Subject" value=""><br><br><br>
-               <textarea name="message" id="" cols="70" rows="20"></textarea><br><br>
-               <label for="validate" class='validate'>Are You Human?</label><br>
-               <label class="validate">
-                  <span>6 + 2 = </span>
-                  <input type="text" name="validate" id="form-validate" class="validate" placeholder="#">
-               </label>
-               <button type="reset" id="cancel">Cancel</button>
-               <button type="submit" id="submit">Submit</button>
+            <div class="form-group">
+               <label for="name">Full Name</label>
+               <!-- Validate input using php handle_email.php -->
+               <input type="text" name="name" id="form-name" placeholder="Full Name" value="<?php echo validate_input('name'); ?>" class="form-control">
+            </div>
+            <div class="form-group">
+               <label for="email">Email Address</label>
+               <!-- Validate input using php handle_email.php -->
+               <input type="email" name="email" id="form-email" placeholder="Email Address" value="<?php echo validate_input('email'); ?>" class="form-control">
+            </div>
+            <div class="form-group">
+               <label for="subject">Subject</label>
+               <!-- Validate input using php handle_email.php -->
+               <input type="text" name="subject" id="form-subject" placeholder="Subject" value="<?php echo validate_input('subject'); ?>" class="form-control">
+            </div>
+            <div class="form-group">
+               <!-- Validate input using php handle_email.php -->
+               <textarea name="message" id="" cols="50" rows="10" class="form-control"><?php echo validate_input('message'); ?></textarea>
+            </div>
+            <label for="validate" class='validate'>Are You Human?</label><br>
+            <label class="validate">
+               <span id="operand-1"><?php echo $operand_1; ?></span><span> + </span><span id="operand-2"><?php echo $operand_2; ?></span><span> = </span>
+               <input type="text" name="validate" id="form-validate" class="validate" placeholder="#">
+            </label>
+            <div class="form-group">
+               <button type="reset" id="cancel" class="btn btn-primary btn-lg">Cancel</button>
+               <button type="submit" id="submit" class="btn btn-primary btn-lg" value="Processing Message!">Submit</button>
+            </div>
          </form>
       </div>
    </main>
--- contact2.php
+++ contact2.php
@@ -1,25 +1,87 @@
 <?php
    require 'header.php';
-?>
-   <main class="container">
-      <h1 id="contact-h1">Contact Us</h1>
-         <div id="contact-form">
-            <div id="form-heading">
-               <p>Have a question, conment, or concern?</p>
-               <p>Leave us a message</p>
-            </div>
-            <form action="#" method="post" class="form" name="contact-form" target="_self">
-               <div class="input-group">
-                  <div class="input-group-prepend">
-                     <span class="input-group-text">First and last name</span>
-                  </div>
-                  <input type="text" aria-label="First name" class="form-control">
-                  <input type="text" aria-label="Last name" class="form-control">
-               </div>  
-            </form>   
-         </div>
-   </main>
+   //require 'php/handle_email.php';
 
+   ini_set('display_errors', 1);
+   error_reporting(E_ALL);
+
+   $operand_1 = rand(0,9);
+   $operand_2 = rand(0,9);
+   $solution = $operand_1 + $operand_2
+   
+   // $errors = array();
+   // $sent = false;
+
+   // if (! empty($_POST)) {
+   //    $process = process_form($_POST);
+
+   //    //Check for Errors
+   //    if (! empty($process['message'])){
+   //       $errors[] = $process['message'];
+   //    } else if (! empty($process['errors'])){
+   //       $errors = $process['errors'];
+   //    } else {
+   //       $sent = true;
+   //    }
+   // }
+?>
+   <main class="container contact-container">
+      <h1 id="contact-h1" class="main-h1">Contact Us</h1>
+      <div id="contact-form">
+         <div id="form-heading">
+            <p>Have a question, conment, or concern?</p>
+            <p>Leave us a message</p>
+         </div>
+         <div id="form-errors">
+            <!-- PHP if statement to determine errors.  If so print the errors, otherwise print success message -->
+            <!-- If there are errors, print the errors -->
+            <div class="errors">
+               <!-- Prints errors to the page -->
+            </div>
+            <!-- put display element script here again -->
+            <!-- If message sent without errors print success message -->
+            <div class="success">
+               <p class="bg-success">Thank You! Your message was sent. We'll be in touch.</p>
+            </div>
+            <!-- When a message is sent this script displays the form error element -->
+            <script>
+               document.querySelector('#form-heading').style.display = 'none';
+               document.querySelector('#form-errors').style.display = "initial";
+            </script>
+
+         </div>
+         <form action="#" method="post" class="form" name="contact-form" target="_self">
+            <div class="form-group">
+               <label for="name">Full Name</label>
+               <!-- Validate input using php handle_email.php -->
+               <input type="text" name="name" id="form-name" placeholder="Full Name" value="<?php if (isset($_POST['name'])) { print $_POST['name']; } ?>" class="form-control">
+            </div>
+            <div class="form-group">
+               <label for="email">Email Address</label>
+               <!-- Validate input using php handle_email.php -->
+               <input type="email" name="email" id="form-email" placeholder="Email Address" value="<?php if (isset($_POST['email'])) { print $_POST['email']; } ?>" class="form-control">
+            </div>
+            <div class="form-group">
+               <label for="subject">Subject</label>
+               <!-- Validate input using php handle_email.php -->
+               <input type="text" name="subject" id="form-subject" placeholder="Subject" value="<?php if (isset($_POST['subject'])) { print $_POST['subject']; } ?>" class="form-control">
+            </div>
+            <div class="form-group">
+               <!-- Validate input using php handle_email.php -->
+               <textarea name="message" id="" cols="50" rows="10" class="form-control"><?php if (isset($_POST['message'])) { print $_POST['message']; } ?></textarea>
+            </div>
+            <label for="validate" class='validate'>Are You Human?</label><br>
+            <label class="validate">
+               <span id="operand-1"><?php echo $operand_1; ?></span><span> + </span><span id="operand-2"><?php echo $operand_2; ?></span><span> = </span>
+               <input type="text" name="validate" id="form-validate" class="validate" placeholder="#">
+            </label>
+            <div class="form-group">
+               <button type="reset" id="cancel" class="btn btn-primary btn-lg">Cancel</button>
+               <button type="submit" id="submit" class="btn btn-primary btn-lg" value="Processing Message!">Submit</button>
+            </div>
+         </form>
+      </div>
+   </main>
 <?php
    require 'footer.php';
 ?>
